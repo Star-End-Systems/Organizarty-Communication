706a54979ffbe34bb30c95a49cd5ffe91787433e
lu-css <dossantossilvalucas963@gmail.com>
2023-12-31 20:15:22 -0300

    [refactor] remove user endpoints

--- src/app.container.ts
+++ src/app.container.ts
@@ -1,7 +1,6 @@
 import { Container } from "inversify";
 import { AppContainer } from "@expressots/core";
 import { AppModule } from "@useCases/app/app.module";
-import { UserModule } from "@useCases/user/user.module";
 import { SendEmailModule } from "@useCases/send-email/send-email.module";
 
 const appContainer = new AppContainer();
@@ -9,6 +8,5 @@ const appContainer = new AppContainer();
 export const container: Container = appContainer.create([
     // Add your modules here
     AppModule,
-    UserModule,
     SendEmailModule,
 ]);
